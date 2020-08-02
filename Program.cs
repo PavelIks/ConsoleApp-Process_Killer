5d0bc3703b55820aff89c1cd17e4e942c5fd22eb
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-08-02 16:26:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ProcessKiller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string ");
+            string str = Console.ReadLine();
+            foreach (Process process in Process.GetProcessesByName(str))
+            {
+                process.Kill();
+                Console.WriteLine("Процесс убит.");
+            }
+            Console.Read();
+
+            /*foreach (Process process in Process.GetProcessesByName("Firefox"))
+            {
+                process.Kill();
+                Console.WriteLine("Процесс убит.");
+            }
+            Console.Read();*/
+        }
+    }
+}
